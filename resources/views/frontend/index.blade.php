6c21f5d212feeb7f464c548ef4a526dd95f4e95e
yohanafitria <yohana_009@smkassalaambandung.sch.id>
2022-08-12 18:04:59 +0700

    Test-PKL-Eksternal

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- SITE TITTLE -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pokemon</title>
+
+    <!-- FAVICON -->
+    <link href="{{asset('assets/img/favicon.png')}}" rel="shortcut icon">
+    <!-- PLUGINS CSS STYLE -->
+    <!-- <link href="plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet"> -->
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/plugins/bootstrap/css/bootstrap-slider.css')}}">
+    <!-- Font Awesome -->
+    <link href="{{asset('assets/plugins/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <!-- Owl Carousel -->
+    <link href="{{asset('assets/plugins/slick-carousel/slick/slick.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/plugins/slick-carousel/slick/slick-theme.css')}}" rel="stylesheet">
+    <!-- Fancy Box -->
+    <link href="{{asset('assets/plugins/fancybox/jquery.fancybox.pack.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/plugins/jquery-nice-select/css/nice-select.css')}}" rel="stylesheet">
+    <!-- CUSTOM CSS -->
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+</head>
+
+<body class="body-wrapper">
+
+    <section style="background-color: ;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-expand-lg navbar-light navigation">
+                        <a class="navbar-brand">
+                            <img src="{{asset('assets/img/logo.jpg')}}" height="50px" width="150px">
+                        </a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <br>
+    <!-- Category list -->
+    <div class="col-lg-4 offset-lg-0 col-md-5 offset-md-1 col-sm-6 col-6">
+        <div class="category-block">
+            <div class="header">
+                <img src="{{asset('assets/img/biru.jpg')}}" height="200px" width="300px">
+            </div>
+            <button type="button" class="btn btn-success">Grass</button>
+            <button type="button" class="btn btn-primary">Poison</button>
+        </div>
+    </div>
+
+    <br>
+    <body class="body-wrapper">
+
+    <section style="background-color: ;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-expand-lg navbar-light navigation">
+                        <a class="navbar-brand">
+                            <img src="{{asset('assets/img/logo.jpg')}}" height="50px" width="150px">
+                        </a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <br>
+    <!-- Category list -->
+    <div class="col-lg-4 offset-lg-0 col-md-5 offset-md-1 col-sm-6 col-6">
+        <div class="category-block">
+            <div class="header">
+                <img src="{{asset('assets/img/kuning.jpg')}}" height="200px" width="300px">
+            </div>
+            <button type="button" class="btn btn-success">Grass</button>
+            <button type="button" class="btn btn-primary">Poison</button>
+        </div>
+    </div>
+    </br>
+
+    <br>
+    <body class="body-wrapper">
+
+    <section style="background-color: ;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-expand-lg navbar-light navigation">
+                        <a class="navbar-brand">
+                            <img src="{{asset('assets/img/logo.jpg')}}" height="50px" width="150px">
+                        </a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <br>
+    <!-- Category list -->
+    <div class="col-lg-4 offset-lg-0 col-md-5 offset-md-1 col-sm-6 col-6">
+        <div class="category-block">
+            <div class="header">
+                <img src="{{asset('assets/img/hijau.jpg')}}" height="200px" width="300px">
+            </div>
+            <button type="button" class="btn btn-success">Grass</button>
+            <button type="button" class="btn btn-primary">Poison</button>
+        </div>
+    </div>
+    </br>
+
+    <br>
+    <body class="body-wrapper">
+
+    <section style="background-color: ;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-expand-lg navbar-light navigation">
+                        <a class="navbar-brand">
+                            <img src="{{asset('assets/img/logo.jpg')}}" height="50px" width="150px">
+                        </a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <br>
+    <!-- Category list -->
+    <div class="col-lg-4 offset-lg-0 col-md-5 offset-md-1 col-sm-6 col-6">
+        <div class="category-block">
+            <div class="header">
+                <img src="{{asset('assets/img/pokemon.jpg')}}" height="200px" width="300px">
+            </div>
+            <button type="button" class="btn btn-success">Grass</button>
+            <button type="button" class="btn btn-primary">Poison</button>
+        </div>
+    </div>
+    </br>
+
+</body>
+
+    <!-- JAVASCRIPTS -->
+    <script src="{{asset('assets/plugins/jQuery/jquery.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/bootstrap/js/popper.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/bootstrap/js/bootstrap-slider.js')}}"></script>
+    <!-- tether js -->
+    <script src="{{asset('assets/plugins/tether/js/tether.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/raty/jquery.raty-fa.js')}}"></script>
+    <script src="{{asset('assets/plugins/slick-carousel/slick/slick.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/jquery-nice-select/js/jquery.nice-select.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/fancybox/jquery.fancybox.pack.js')}}"></script>
+    <script src="{{asset('assets/plugins/smoothscroll/SmoothScroll.min.js')}}"></script>
+    <!-- google map -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU&libraries=places"></script>
+    <script src="{{asset('assets/plugins/google-map/gmap.js')}}"></script>
+    <script src="{{asset('assets/js/script.js')}}"></script>
+
+</body>
+
+</html>
